6cc398a368bc4a0700130a588ff31427c8b4c42a
Obada <obada_darkznly@hotmail.com>
2021-01-22 13:30:39 +0200

    Created the gallery item model

---
+++ GridList/GridList/Classes/Models/GalleryItem.swift
@@ -0,0 +1,18 @@
+//
+//  GalleryItem.swift
+//  GridList
+//
+//  Created by Obada on 1/22/21.
+//
+
+import Foundation
+
+
+
+struct GalleryItem: Codable {
+    
+    // Mark: Properties
+    let image: String
+    let title: String
+    let desctiption: String
+}
