e035af69de947ccc063f636a80bb86fcba27043d
Obada <obada_darkznly@hotmail.com>
2021-01-22 12:42:52 +0200

    Created the tab bar

---
+++ GridList/GridList/Classes/Controllers/TabViewController.swift
@@ -0,0 +1,17 @@
+//
+//  TabViewController.swift
+//  GridList
+//
+//  Created by Obada on 1/22/21.
+//
+
+import UIKit
+
+class TabViewController: UITabBarController, UITabBarControllerDelegate {
+    
+    // MARK: Controller's life cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Tab bar opened")
+    }
+}
